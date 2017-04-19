dc56420a6bf3887fd8ca0d977547ca3cca4e178a
MrValdez <shunyvaldez@gmail.com>
2017-04-19 20:50:59 +0800

    more refactoring for drawing pad in preparation for sending initial drawings via network

--- drawingpad.py
+++ drawingpad.py
@@ -2,21 +2,27 @@ import pygame
 
 pen_color = [0, 0, 0]
 drawing_pad_color = [255, 255, 155]
+drawing_size = [500, 500]
+
 
 class pad:
-    def __init__(self):
-
-        drawing_size = [500, 500]
+    def __init__(self, pos, scale=1):
         self.surface = pygame.Surface(drawing_size)
+        self.scale = scale
         self.surface.fill(drawing_pad_color)
-        self.pos = [40, 40]
+        self.pos = pos
 
     def draw(self, screen):
+        scaled_size = list(map(lambda x: int(x * self.scale), drawing_size))
+        surface = pygame.transform.scale(self.surface, scaled_size)
+
+        # draw border
         pygame.draw.rect(screen,
                          [0, 0, 0],
-                         self.surface.get_rect().move(self.pos),
-                         10)  # draw border
-        screen.blit(self.surface, self.pos)
+                         surface.get_rect().move(self.pos),
+                         10)
+
+        screen.blit(surface, self.pos)
 
     def draw_brush(self, pos):
         self.apply_brush(pos, pen_color)
--- main.py
+++ main.py
@@ -12,7 +12,9 @@ screen = pygame.display.set_mode(resolution)
 pygame.display.set_caption("Pictionary clone")
 clock = pygame.time.Clock()
 
-main_pad = pad()
+main_pad = pad([40, 40])
+p2_pad = pad([700, 40], scale=.45)
+p3_pad = pad([700, 300], scale=.45)
 
 drawing_delta = 0
 
@@ -23,15 +25,11 @@ while isRunning:
     screen.fill([255, 255, 255])
 
     main_pad.draw(screen)
+    p2_pad.draw(screen)
+    p3_pad.draw(screen)
 
     mouse_down = pygame.mouse.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
-
-    network_data = client.update()
-    if network_data:
-        mouse_pos = network_data
-        mouse_down = list(mouse_down)
-        mouse_down[0] = True
 
     if mouse_down[0]:
         main_pad.draw_brush(mouse_pos)
@@ -42,6 +40,18 @@ while isRunning:
     if mouse_down[0] or mouse_down[2]:
         mouse_movements = pygame.mouse.get_rel()
         drawing_delta += sum(map(abs, mouse_movements))
+
+    network_data = client.update()
+    if network_data:
+        mouse_pos = network_data
+        mouse_down = list(mouse_down)
+        mouse_down[0] = True
+
+    if mouse_down[0]:
+        p2_pad.draw_brush(mouse_pos)
+
+    if mouse_down[2]:
+        p2_pad.erase_brush(mouse_pos)
 
     pygame.display.update()
     clock.tick(60)
